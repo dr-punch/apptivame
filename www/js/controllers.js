7ea02b1a2d958e1a31a484973ab2b8e281202ed3
dr-punch <user@Joses-MacBook-Pro.local>
2015-12-13 10:51:00 -0500

    Inicio

---
+++ www/js/controllers.js
@@ -0,0 +1,90 @@
+angular.module('app.controllers', [])
+  
+.controller('apptíVameCtrl', function($scope) {
+
+})
+   
+.controller('homeCtrl', function($scope) {
+
+})
+   
+.controller('listadoArteCtrl', ['$scope','$http','$state',function($scope,$http,$state) {
+    $http.get('js/actividadesculturales.json')
+    .success(function(data){
+        console.log("ListadoARte:" + data);
+        $scope.listadosvc = data.result;
+    })
+    .error(function(data){
+        console.log("error:"+ data);
+    });
+
+}])
+
+
+.controller('listadoCineCtrl', ['$scope','$http','$state',function($scope,$http,$state) {
+    $http.get('js/cine.json')
+    .success(function(data){
+        console.log("ListadoCine:" + data);
+        $scope.listadosvc = data.result;
+    })
+    .error(function(data){
+        console.log("error:"+ data);
+    });
+
+}])
+.controller('listadoDanzaCtrl', ['$scope','$http','$state',function($scope,$http,$state) {
+    $http.get('js/danza.json')
+    .success(function(data){
+        console.log("ListadoDanza:" + data);
+        $scope.listadosvc = data.result;
+    })
+    .error(function(data){
+        console.log("error:"+ data);
+    });
+
+}])
+
+.controller('listadoVetCtrl', ['$scope','$http','$state',function($scope,$http,$state) {
+    $http.get('js/vet.json')
+    .success(function(data){
+        console.log("ListadoVet:" + data);
+        $scope.listadosvc = data.result;
+    })
+    .error(function(data){
+        console.log("error:"+ data);
+    });
+
+}])
+
+controller('listadoComidaSanaCtrl', ['$scope','$http','$state',function($scope,$http,$state) {
+    $http.get('js/comidasana.json')
+    .success(function(data){
+        console.log("ListadoComidaSana:" + data);
+        $scope.listadosvc = data.result;
+    })
+    .error(function(data){
+        console.log("error:"+ data);
+    });
+
+}])
+
+.controller('eventosCtrl', ['$scope','$http','$state',function($scope,$http,$state) {
+    
+    $http.get('js/actividadesculturales.json')
+    .success(function(data){
+        console.log("actividadesculturales:" + data);
+        $scope.listadosvc = data.result;
+    })
+    .error(function(data){
+        console.log("error:"+ data);
+    });
+}])
+   
+.controller('detalleCtrl', function($scope) {
+
+})
+   
+.controller('mensajeCtrl', function($scope) {
+
+})
+ 
---
+++ www/js/routes.js
@@ -0,0 +1,91 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+      
+        
+    .state('apptíVame', {
+      url: '/page1',
+      templateUrl: 'templates/apptíVame.html',
+      controller: 'apptíVameCtrl'
+    })
+        
+      
+    
+      
+        
+    .state('home', {
+      url: '/page2',
+      templateUrl: 'templates/home.html',
+      controller: 'homeCtrl'
+    })
+        
+      
+    
+      
+        
+    .state('listado', {
+      url: '/page3',
+      templateUrl: 'templates/listado.html',
+      controller: 'listadoCtrl'
+    })
+  
+    .state('listadoArte', {
+      url: '/listadoArte',
+      templateUrl: 'templates/listadoArte.html',
+      controller: 'listadoArteCtrl'
+    })
+        
+    .state('listadoCine', {
+      url: '/listadoCine',
+      templateUrl: 'templates/listadoCine.html',
+      controller: 'listadoCineCtrl'
+    })
+    
+  .state('listadoDanza', {
+      url: '/listadoDanza',
+      templateUrl: 'templates/listadoDanza.html',
+      controller: 'listadoDanzaCtrl'
+    })
+  
+  .state('listadoVet', {
+      url: '/listadoVet',
+      templateUrl: 'templates/listadoVet.html',
+      controller: 'listadoVetCtrl'
+    })
+
+  .state('listadoComidaSana', {
+      url: '/listadoComidaSana',
+      templateUrl: 'templates/listadoComidaSana.html',
+      controller: 'listadoComidaSanaCtrl'
+    })
+        
+    .state('detalle', {
+      url: '/page4',
+      templateUrl: 'templates/detalle.html',
+      controller: 'detalleCtrl'
+    })
+        
+      
+    
+      
+        
+    .state('mensaje', {
+      url: '/page5',
+      templateUrl: 'templates/mensaje.html',
+      controller: 'mensajeCtrl'
+    })
+        
+      
+    ;
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/page1');
+
+});
